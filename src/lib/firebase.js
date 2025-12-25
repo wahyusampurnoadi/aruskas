3e936ef8d142374142f9d7ac427c2588f41c1a05
Wahyu Sampurno Adi <wahyusampurnoadi@gmail.com>
2025-12-26 02:43:08 +0700

    Final check on firebase config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD8wia_XWRGdTmpjXCmfmvPDF3tdNTyLfM",
-  authDomain: "aruskas-app.firebaseapp.com",
-  projectId: "aruskas-app",
-  storageBucket: "aruskas-app.firebasestorage.app",
-  messagingSenderId: "491358319250",
-  appId: "1:491358319250:web:b70f67f4f54d1895c62c4b",
-  measurementId: "G-FNXBDXZVYQ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
